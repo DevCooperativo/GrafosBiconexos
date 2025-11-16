12ba493f54d41b5f0b15bfb29a411f8a18da2267
LFerriVM <lucasfvm60@gmail.com>
2025-11-16 16:06:55 -0300

    initital project

---
+++ src/GrafosBiconexos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GrafosBiconexos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
